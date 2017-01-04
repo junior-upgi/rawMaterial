34e9601dfe09c3aacd7418fe7e64c8df46244316
Chia-Yu Tsai <Chia-Yu Tsai>
2017-01-04 15:23:39 +0800

    rawMaterial.js
    expand information according to purchase order contents

--- src/backend/model/rawMaterial.js
+++ src/backend/model/rawMaterial.js
@@ -2,39 +2,84 @@ const list = [{
     PRD_NO: 'MROPA001',
     NAME: '石英砂',
     UT: 'KG',
-    type: ['dry', '2% moisture'],
-    transMethod: [],
+    type: [{
+        reference: '石英濕砂',
+        specification: '散裝',
+        unitPrice: null
+    }, {
+        reference: '石英乾砂',
+        specification: '散裝',
+        unitPrice: null
+    }, {
+        reference: '石英乾砂',
+        specification: '散裝',
+        unitPrice: null
+    }, {
+        reference: '石英乾砂',
+        specification: '太空包',
+        unitPrice: null
+    }],
     supplier: ['FH02']
 }, {
     PRD_NO: 'MROPH001',
     NAME: '純鹼',
     UT: 'KG',
-    transMethod: [],
+    type: [{
+        reference: '純鹼',
+        specification: null,
+        unitPrice: null
+    }],
     supplier: ['HH05', 'DJ02']
 }, {
     PRD_NO: 'MROPC001',
     NAME: '芒硝',
     UT: 'KG',
-    transMethod: [],
+    type: [{
+        reference: '芒硝',
+        specification: '25KG裝/針織袋',
+        unitPrice: null
+    }],
     supplier: ['SG01']
 }, {
     PRD_NO: 'MROPA003',
     NAME: '螢石粉',
     UT: 'KG',
-    transMethod: [],
+    type: [{
+        reference: '螢石粉',
+        specification: '1,250公斤/太空包',
+        unitPrice: null
+    }],
     supplier: ['SI13']
 }, {
     PRD_NO: 'MROPD001',
     NAME: '石灰石粉',
     UT: 'KG',
-    transMethod: [],
+    type: [{
+        reference: '石灰鈣粒(石粉)',
+        specification: '14目改7目',
+        unitPrice: null
+    }],
     supplier: ['GC01']
 }, {
     PRD_NO: 'MROPD002',
     NAME: '白雲石粉',
     UT: 'KG',
-    transMethod: [],
+    type: [{
+        reference: '白雲石粉',
+        specification: '1500KG/太空包\n粒度:粗 (8~20 mesh)',
+        unitPrice: null
+    }],
     supplier: ['GC01']
+}, {
+    PRD_NO: 'MROPA006',
+    NAME: '鈉長石',
+    UT: 'KG',
+    type: [{
+        reference: '鈉長石\n(Premier 600)',
+        specification: '1,200KG/太空包 2包/板 10板/櫃',
+        unitPrice: null
+    }],
+    supplier: ['JJ07']
 }];
 
 module.exports = {
