3889a648331c2f1fae99acbb27a1b8a8ee7e0078
Chia-Yu Tsai <Chia-Yu Tsai>
2017-02-15 08:05:17 +0800

    batch reservation 1. added buttons with badge to show existing reservation 2. added input box to make reservation 3. removed the spinner to prevent excessive number of change events being registered with each spinner press

--- src/frontend/component/userInterface/reservationCell.vue
+++ src/frontend/component/userInterface/reservationCell.vue
@@ -1,7 +1,20 @@
 <template>
     <div>
         <div>{{cellDate.format('M/D')}}</div>
-        <div v-if="shipment!==undefined">{{shipment.CUS_NO}}</div>
+        <br>
+        <div v-if="shipment!==undefined">
+            <button type="button" class="btn btn-danger btn-xs">
+                取消預約: <span class="badge">{{shipment.quantity}}</span>
+            </button>
+        </div>
+        <div v-else class="">
+            <input
+                type="number"
+                min="1"
+                class="form-control input-sm col-sm-2 text-center"
+                v-model="quantity"
+                @change="processReservation" />
+        </div>
     </div>
 </template>
 
@@ -15,9 +28,31 @@
         ],
         data: function() {
             return {
-                cellDate: moment(this.cellDateString)
+                cellDate: moment(this.cellDateString),
+                quantity: null
             };
+        },
+        watch: {
+            quantity: function(newValue) {
+                if (newValue === '') {
+                    this.quantity = null;
+                }
+            }
+        },
+        methods: {
+            processReservation: function() {
+                console.log('new res');
+            }
         }
     };
 
 </script>
+
+<style>
+    input[type=number]::-webkit-inner-spin-button,
+    input[type=number]::-webkit-outer-spin-button {
+        -webkit-appearance: none;
+        margin: 0;
+    }
+
+</style>
