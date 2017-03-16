f67db1d82a5374ce570c228e03df40f43a506fb4
Chia-Yu Tsai <Chia-Yu Tsai>
2017-03-16 18:56:03 +0800

    furnace module added a shipmentOverview component

--- src/frontend/component/furnace/furnace.vue
+++ src/frontend/component/furnace/furnace.vue
@@ -2,18 +2,21 @@
     <div class="text-center col-xs-12 col-sm-10">
         <h2>窯爐模組</h2>
         <br>
+        <shipmentOverview></shipmentOverview>
         <batch-reservation></batch-reservation>
         <shipmentSchedule></shipmentSchedule>
     </div>
 </template>
 
 <script>
+    import shipmentOverview from '../purchasing/shipmentOverview/shipmentOverview.vue';
     import batchReservation from './batchReservation/batchReservation.vue';
     import shipmentSchedule from './shipmentSchedule/shipmentSchedule.vue';
 
     export default {
         name: 'furnace',
         components: {
+            shipmentOverview,
             batchReservation,
             shipmentSchedule
         }
