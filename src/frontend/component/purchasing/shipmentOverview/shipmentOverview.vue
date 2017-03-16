159a97be7f6ae76d855a1105d308f2a04ce291f4
Chia-Yu Tsai <Chia-Yu Tsai>
2017-03-17 07:09:21 +0800

    shipmentOverview
    display tweak

--- src/frontend/component/purchasing/shipmentOverview/shipmentOverview.vue
+++ src/frontend/component/purchasing/shipmentOverview/shipmentOverview.vue
@@ -2,26 +2,30 @@
     <div class="panel panel-primary">
         <div class="panel-heading">
             <h4>
-                    <workingTimeSelector></workingTimeSelector>
-                    &nbsp;原料進廠概況&nbsp;&nbsp;&nbsp;
-                    <span style="border:1px solid white;" class="label label-primary">今日</span>
-                    <span style="border:1px solid white;" class="label label-danger">待進廠</span>
-                    <span style="border:1px solid white;" class="label label-success">已入廠</span>
-                </h4>
+                        <workingTimeSelector></workingTimeSelector>
+                        &nbsp;原料進廠概況&nbsp;&nbsp;&nbsp;
+                        <span style="border:1px solid white;" class="label label-success">已入廠</span>
+                        <span style="border:1px solid white;" class="label label-danger">待進廠</span>
+                        <span style="border:1px solid white;" class="label label-primary">今日</span>
+                    </h4>
         </div>
         <div class="table-responsive">
             <table class="table table-striped table-bordered table-condensed table-hover">
                 <thead>
                     <tr class="bg-warning">
-                        <th class="text-center" style="white-space:nowrap;">廠商</th>
-                        <th class="text-center" style="white-space:nowrap;">項目</th>
+                        <th class="text-center"
+                            style="white-space:nowrap;">廠商</th>
+                        <th class="text-center"
+                            style="white-space:nowrap;">項目</th>
                         <th class="text-center">入廠日期</th>
-                        <th class="text-center" style="white-space:nowrap;">車次</th>
+                        <th class="text-center"
+                            style="white-space:nowrap;">車次</th>
                         <th class="text-center">重量</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <shipmentOverviewRecord v-for="(monthlyShipmentOverview, index) in releventMonthlyShipmentOverview" :monthlyShipmentOverview="monthlyShipmentOverview">
+                    <shipmentOverviewRecord v-for="(monthlyShipmentOverview, index) in releventMonthlyShipmentOverview"
+                                            :monthlyShipmentOverview="monthlyShipmentOverview">
                     </shipmentOverviewRecord>
                 </tbody>
             </table>
