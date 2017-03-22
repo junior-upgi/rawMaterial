b8e3864d447c4cc1eaea95e98940862ce102e395
Chia-Yu Tsai <Chia-Yu Tsai>
2017-03-22 14:05:46 +0800

    reminderDisplay component fix the text in the textarea caption is printed when the textarea is empty

--- src/frontend/component/purchasing/purchaseOrder/pORenderViewPort/reminderDisplay.vue
+++ src/frontend/component/purchasing/purchaseOrder/pORenderViewPort/reminderDisplay.vue
@@ -9,7 +9,11 @@
                 </div>
             </td>
             <td colspan="4" class="text-right">
-                <textarea style="padding-left:10px;width:100%;height:100%;resize:vertical;border:0px;" rows="8" placeholder="可在此手動輸入其他資訊。訊息可列印但不加以儲存 (高度調整可能超過單頁列印尺寸，請自行斟酌輸入內容多寡)" v-model.lazy="customMessageValue" @change="$emit('customMessageChangeEvent', customMessageValue)">
+                <textarea
+                    style="padding-left:10px;width:100%;height:100%;resize:vertical;border:0px;" rows="8"
+                    :placeholder="customMessagePlaceholder"
+                    v-model.lazy="customMessageValue"
+                    @change="$emit('customMessageChangeEvent', customMessageValue)">
                 </textarea>
             </td>
         </tr>
@@ -23,14 +27,19 @@ export default {
     name: 'reminderDisplay',
     props: ['customMessage', 'pONoticeList'],
     computed: {
-        ...mapGetters({
-            pOPrintMode: 'checkPOPrintMode'
-        }),
+        ...mapGetters({ pOPrintMode: 'checkPOPrintMode' }),
         printingBorder: function() {
             if (this.pOPrintMode) {
                 return '2px solid black !important';
             } else {
                 return null;
+            }
+        },
+        customMessagePlaceholder: function() {
+            if (this.pOPrintMode) {
+                return '';
+            } else {
+                return '可在此手動輸入其他資訊。訊息可列印但不加以儲存 (高度調整可能超過單頁列印尺寸，請自行斟酌輸入內容多寡)';
             }
         }
     },
