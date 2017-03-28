3dc0d0a324e8a7b5f7c18475002c8b4257afc7bc
Chia-Yu Tsai <Chia-Yu Tsai>
2017-03-28 13:45:04 +0800

    code cleanup remove deprecated code

--- src/backend/route/data/rawMaterial.js
+++ src/backend/route/data/rawMaterial.js
@@ -42,34 +42,4 @@ router.get('/data/rawMaterial/knownList', tokenValidation, (request, response) =
         });
 });
 
-/*
-router.get('/data/rawMaterial/supplyingSpecList', tokenValidation, (request, response) => {
-    const knex = require('knex')(serverConfig.mssqlConfig);
-    knex.select('*')
-        .from('rawMaterial.dbo.supplyingSpecList')
-        .where({
-            workingYear: request.query.workingYear,
-            workingMonth: request.query.workingMonth
-        })
-        .orderBy('CUS_NO')
-        .orderBy('PRD_NO')
-        .orderBy('typeId')
-        .debug(false)
-        .then((resultset) => {
-            return response.status(200).json({ supplyingSpecList: resultset });
-        })
-        .catch((error) => {
-            return response.status(500).json(
-                utility.endpointErrorHandler(
-                    request.method,
-                    request.originalUrl,
-                    `原料進貨規格列表相關資料讀取發生錯誤: ${error}`)
-            );
-        })
-        .finally(() => {
-            knex.destroy();
-        });
-});
-*/
-
 module.exports = router;
