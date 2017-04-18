3e90254b478490f219d4845ab1e78723fe0a7fae
Chia-Yu Tsai <Chia-Yu Tsai>
2017-04-18 16:55:47 +0800

    generalSection component
    increased font size at several area

--- src/frontend/component/purchasing/purchaseOrder/pORenderViewPort/generalSection.vue
+++ src/frontend/component/purchasing/purchaseOrder/pORenderViewPort/generalSection.vue
@@ -5,7 +5,8 @@
                 <tr>
                     <td class="bg-primary" :style="{border:printingBorder}">廠 商 名 稱</td>
                     <td
-                        class="text-left" style="padding-left:20px;right-padding:0px;"
+                        class="text-left"
+                        style="font-size:150%;padding-left:20px;right-padding:0px;"
                         :style="{border:printingBorder}">{{activePO.supplier.NAME}}</td>
                     <td class="bg-primary" :style="{border:printingBorder}">訂 單 編 號</td>
                     <td
@@ -16,17 +17,24 @@
                 </tr>
                 <tr>
                     <td class="bg-primary" :style="{border:printingBorder}">業 務 聯 絡 人</td>
-                    <td class="text-left" style="padding-left:20px;right-padding:0px;" :style="{border:printingBorder}">
+                    <td class="text-left"
+                        style="font-size:150%;padding-left:20px;right-padding:0px;"
+                        :style="{border:printingBorder}">
                         <span>{{activePO.supplier.CNT_MAN1}}</span>
                         <span v-if="activePO.supplier.CNT_MAN2!==null">/</span>
                         <span v-if="activePO.supplier.CNT_MAN2!==null">{{activePO.supplier.CNT_MAN2}}</span>
                     </td>
                     <td class="bg-primary" :style="{border:printingBorder}">訂 貨 日 期</td>
-                    <td :style="{border:printingBorder}">{{activePO.documentDate}}</td>
+                    <td :style="{border:printingBorder}"
+                        style="font-size:150%;">
+                        {{activePO.documentDate}}
+                    </td>
                 </tr>
                 <tr>
                     <td class="bg-primary" :style="{border:printingBorder}">聯 絡 電 話</td>
-                    <td class="text-left" style="padding-left:20px;right-padding:0px;" :style="{border:printingBorder}">
+                    <td class="text-left"
+                        style="font-size:150%;padding-left:20px;right-padding:0px;"
+                        :style="{border:printingBorder}">
                         <span style="white-space:nowrap;">{{activePO.supplier.TEL1}}</span>
                         <span v-if="activePO.supplier.TEL2!==null">/</span>
                         <span style="white-space:nowrap;" v-if="activePO.supplier.TEL2!==null">{{activePO.supplier.TEL2}}</span>
@@ -40,12 +48,15 @@
                 <tr>
                     <td class="bg-primary" :style="{border:printingBorder}">傳 真 電 話</td>
                     <td class="text-left"
-                        style="padding-left:20px;right-padding:0px;"
+                        style="font-size:150%;padding-left:20px;right-padding:0px;"
                         :style="{border:printingBorder}">
                         {{activePO.supplier.FAX}}
                     </td>
                     <td class="bg-primary" :style="{border:printingBorder}">採 購 經 辦</td>
-                    <td :style="{border:printingBorder}">{{userInfo.NAME}} 分機: {{userInfo.compPhoneExt}}</td>
+                    <td :style="{border:printingBorder}"
+                        style="font-size:150%;white-space:nowrap;">
+                        {{userInfo.NAME}} 分機: {{userInfo.compPhoneExt}}
+                    </td>
                 </tr>
             </tbody>
         </table>
